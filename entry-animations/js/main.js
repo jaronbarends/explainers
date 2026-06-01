7ab275b73a5be9c02f1b2e307d735e9a431747da
jaronbarends <731645+jaronbarends@users.noreply.github.com>
2026-06-01 19:53:03 +0200

    add keyframe animations

--- entry-animations/js/main.js
+++ entry-animations/js/main.js
@@ -1,11 +1,33 @@
 initToggleButtons();
+initKeyFrameButton();
 
 function initToggleButtons() {
   const buttons = document.querySelectorAll('button');
-  buttons.forEach(button => {
-    button.addEventListener('click', () => {
-      const box = document.getElementById(button.getAttribute('aria-controls'));
-      box.classList.toggle('is-visible');
-    });
+  buttons.forEach((button) => {
+    button.addEventListener('click', handleToggle);
   });
+}
+
+function handleToggle(e) {
+  const button = e.currentTarget;
+  const box = document.getElementById(button.getAttribute('aria-controls'));
+  box.classList.toggle('is-visible');
+}
+
+function initKeyFrameButton() {
+  const button = document.getElementById('key-frame-button');
+  button.removeEventListener('click', handleToggle);
+  button.addEventListener('click', handleKeyFrameToggle);
+}
+
+function handleKeyFrameToggle(e) {
+  const button = e.currentTarget;
+  const box = document.getElementById(button.getAttribute('aria-controls'));
+  if (box.classList.contains('is-visible')) {
+    box.classList.remove('is-visible');
+    box.classList.add('is-hidden');
+  } else {
+    box.classList.remove('is-hidden');
+    box.classList.add('is-visible');
+  }
 }
