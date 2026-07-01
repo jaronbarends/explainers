32409f6640fb0af20b46482d88edb001a239e19f
jaronbarends <731645+jaronbarends@users.noreply.github.com>
2026-07-01 15:40:39 +0200

    add vertical size

--- container-queries/js/main.js
+++ container-queries/js/main.js
@@ -1,5 +1,5 @@
 initResetButton();
-initContainerWidths();
+initContainerSizes();
 
 function initResetButton() {
   const btn = document.getElementById('reset-btn');
@@ -13,32 +13,45 @@ function resetContainerSizes() {
   });
 }
 
-function initContainerWidths() {
+function initContainerSizes() {
   const resizers = document.querySelectorAll('.resizer');
 
   const observer = new ResizeObserver((entries) => {
     for (const entry of entries) {
-      updateContainerWidths(entry.target);
+      updateContainerSizes(entry.target);
     }
   });
 
   resizers.forEach((resizer) => {
     observer.observe(resizer);
-    updateContainerWidths(resizer);
+    updateContainerSizes(resizer);
   });
 }
 
-function updateContainerWidths(resizer) {
+function updateContainerSizes(resizer) {
   const containers = resizer.querySelectorAll('[class*="container"]');
   containers.forEach((container) => {
-    // const widthPx = container.getBoundingClientRect().width;
-    const style = getComputedStyle(container);
-    const paddingX = parseFloat(style.paddingLeft) + parseFloat(style.paddingRight);
-    const borderX = parseFloat(style.borderLeftWidth) + parseFloat(style.borderRightWidth);
-    const widthPx = container.getBoundingClientRect().width - paddingX - borderX;
-    const widthRem = Math.round((10 * widthPx) / 16) / 10;
-    container.dataset.width = `⟷ ${widthRem}rem`;
-    // container.dataset.width = `width:<> ${widthRem}rem`;
-    // container.dataset.width = widthPx;
+    container.dataset.width = getContainerWidth(container);
+    if (resizer.hasAttribute('data-resizer-size')) {
+      container.dataset.height = getContainerHeight(container);
+    }
   });
+}
+
+function getContainerWidth(container) {
+  const style = getComputedStyle(container);
+  const paddingX = parseFloat(style.paddingLeft) + parseFloat(style.paddingRight);
+  const borderX = parseFloat(style.borderLeftWidth) + parseFloat(style.borderRightWidth);
+  const widthPx = container.getBoundingClientRect().width - paddingX - borderX;
+  const widthRem = Math.round((10 * widthPx) / 16) / 10;
+  return `⟷ ${widthRem}rem`;
+}
+
+function getContainerHeight(container) {
+  const style = getComputedStyle(container);
+  const paddingY = parseFloat(style.paddingTop) + parseFloat(style.paddingBottom);
+  const borderY = parseFloat(style.borderTopWidth) + parseFloat(style.borderBottomWidth);
+  const heightPx = container.getBoundingClientRect().height - paddingY - borderY;
+  const heightRem = Math.round((10 * heightPx) / 16) / 10;
+  return `↕ ${heightRem}rem`;
 }
