5f12cb442bc654e9956aa7c34f5af2698b0b8a15
jaronbarends <731645+jaronbarends@users.noreply.github.com>
2026-07-01 15:23:40 +0200

    add current width

--- container-queries/js/main.js
+++ container-queries/js/main.js
@@ -1,4 +1,5 @@
 initResetButton();
+initContainerWidths();
 
 function initResetButton() {
   const btn = document.getElementById('reset-btn');
@@ -9,5 +10,35 @@ function resetContainerSizes() {
   const containers = document.querySelectorAll('[data-resizer]');
   containers.forEach((container) => {
     container.style.width = 'auto';
+  });
+}
+
+function initContainerWidths() {
+  const resizers = document.querySelectorAll('.resizer');
+
+  const observer = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      updateContainerWidths(entry.target);
+    }
+  });
+
+  resizers.forEach((resizer) => {
+    observer.observe(resizer);
+    updateContainerWidths(resizer);
+  });
+}
+
+function updateContainerWidths(resizer) {
+  const containers = resizer.querySelectorAll('[class*="container"]');
+  containers.forEach((container) => {
+    // const widthPx = container.getBoundingClientRect().width;
+    const style = getComputedStyle(container);
+    const paddingX = parseFloat(style.paddingLeft) + parseFloat(style.paddingRight);
+    const borderX = parseFloat(style.borderLeftWidth) + parseFloat(style.borderRightWidth);
+    const widthPx = container.getBoundingClientRect().width - paddingX - borderX;
+    const widthRem = Math.round((10 * widthPx) / 16) / 10;
+    container.dataset.width = `⟷ ${widthRem}rem`;
+    // container.dataset.width = `width:<> ${widthRem}rem`;
+    // container.dataset.width = widthPx;
   });
 }
